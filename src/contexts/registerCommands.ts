e2244ad3a82d4633fab9a37d32a7ebb3d7cb7053
Andrew Butson <butsona@arbs.io>
2023-03-11 23:42:48 +0100

    remove chat command and reg

--- src/contexts/registerCommands.ts
+++ src/contexts/registerCommands.ts
@@ -2,13 +2,11 @@ import { ExtensionContext } from 'vscode'
 import { registerCompletionCommand } from './registerCompletionCommand'
 import { registerApiKeyCommand } from './registerApiKeyCommand'
 import { registerDefaultModel } from './registerDefaultModel'
-import { registerShowClaimsetPreviewCommand } from './registerGptChatPanelCommand'
 import { registerSidebarProvider } from './registerSidebarProvider'
 
 export function registerCommands(context: ExtensionContext) {
   registerApiKeyCommand(context)
   registerDefaultModel(context)
   registerCompletionCommand(context)
-  registerShowClaimsetPreviewCommand(context)
   registerSidebarProvider(context)
 }

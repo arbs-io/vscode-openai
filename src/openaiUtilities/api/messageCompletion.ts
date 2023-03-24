29339bfe43f62b6e1d1d2d0e7210c637b6fc134d
Andrew Butson <butsona@arbs.io>
2023-03-24 22:22:35 +0100

    remove params that have suitable default (based on model)

--- src/openaiUtilities/api/messageCompletion.ts
+++ src/openaiUtilities/api/messageCompletion.ts
@@ -57,9 +57,7 @@ export async function messageCompletion(
       {
         model: requestConfig.defaultModel,
         messages: chatCompletions,
-        temperature: 0.1,
-        max_tokens: 2048,
-        top_p: 1,
+        temperature: 0.2,
         frequency_penalty: 0.5,
         presence_penalty: 0.5,
       },

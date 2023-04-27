d4fa15d7cb8b2a913e1d42c415a488c8f20b91cb
Andrew Butson <butsona@arbs.io>
2023-04-27 23:42:57 +0200

    fixing design patterns to only output one solution

--- src/utilities/openai/prompt/patternPrompt.ts
+++ src/utilities/openai/prompt/patternPrompt.ts
@@ -9,7 +9,7 @@ export async function patternPrompt(): Promise<string> {
   const inputCode = getActiveTextEditorValue()
 
   const persona = `vscode-openai is a programming expert in ${language}.\n`
-  const request = `Given the following code, rewrite the code using appropriate design patterns. The design pattern to Concurrency, Behavioral, Structural and Creational patterns. Examples like Factory, Builder, Mediator, ... \n`
+  const request = `Given the following code, rewrite the code using appropriate design patterns. The design pattern should be a combination of Concurrency, Behavioral, Structural and Creational design patterns. The response should be a single solution using all the useful patterns identified\n`
   const sourceCode = `\n${inputCode}\n\n`
   const rules = `vscode-openai response must only using valid source code for ${language} programming language.`
   const prompt = persona.concat(rules, request, sourceCode)

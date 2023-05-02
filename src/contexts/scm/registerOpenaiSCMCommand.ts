76313f1dd2082986adf7cb858797343c90825a35
Andrew Butson <butsona@arbs.io>
2023-05-02 23:09:19 +0200

    git comments are now generated via openai

--- src/contexts/scm/registerOpenaiSCMCommand.ts
+++ src/contexts/scm/registerOpenaiSCMCommand.ts
@@ -1,50 +1,91 @@
-//https://github.com/microsoft/vscode/issues/108046
-import {
-  commands,
-  ExtensionContext,
-  extensions,
-  scm,
-  Uri,
-  workspace,
-} from 'vscode'
+// https://github.com/bendera/vscode-commit-message-editor/blob/91ed2bf9c68c4d36cad0f99268e2f4e99083a2e8/src/utils/GitService.ts
+import { commands, ExtensionContext } from 'vscode'
 import { VSCODE_OPENAI_SCM } from '@app/contexts'
-import { showMessageWithTimeout } from '@app/utilities/vscode'
-// import { getGitApi } from './gitHelper'
-import * as vscodegit from './vendor/git'
+import GitService from '@app/utilities/git/gitService'
+import { IChatCompletion, IConversation } from '@app/interfaces'
+import { getSystemPersonas } from '@app/models'
+import { ConversationService } from '@app/services'
+import { createChatCompletion, ResponseFormat } from '@app/utilities/openai'
 
 export function registerOpenaiSCMCommand(context: ExtensionContext) {
   _registerOpenaiSCMCommand(context)
 }
 
 function _registerOpenaiSCMCommand(context: ExtensionContext) {
+  const gitService = new GitService()
+  // gitService.onRepositoryDidChange(handlerRepositoryDidChange)
+
   context.subscriptions.push(
     commands.registerCommand(VSCODE_OPENAI_SCM.COMMENT_COMMAND_ID, async () => {
-      showMessageWithTimeout('working...', 1000)
-
-      const repo = getGitAPI()
-      repo.repositories[0].inputBox.value = 'openai...'
-
-      repo.repositories[0].ui.onDidChange((e) => {
-        repo.repositories[0].ui.selected
-        showMessageWithTimeout(
-          `ui ${repo.repositories[0].rootUri.path}  ${repo.repositories[0].ui.selected}`,
-          10000
-        )
-        console.log('repo', e)
-      })
-      repo.repositories[0].state.onDidChange((e) => {
-        showMessageWithTimeout(`repo ${e}`, 1000)
-        console.log('repo', e)
-      })
+      const differences = await getGitDifferences(gitService)
+      if (differences) {
+        const comments = await getComments(differences)
+        gitService.setSCMInputBoxMessage(comments)
+      }
     })
   )
 }
 
-function getGitAPI(): vscodegit.API {
-  const vscodeGit = extensions.getExtension('vscode.git')
-  if (!vscodeGit?.exports.getAPI(1)) {
-    showMessageWithTimeout('failed to get api...', 1000)
-    // output.error('getGitAPI', getSourcesLocalize('vscodeGitNotFound'), true)
+const getComments = async (diff: string): Promise<string> => {
+  const persona = getSystemPersonas().find(
+    (a) => a.roleName === 'Developer/Programmer'
+  )
+  if (persona) {
+    const conversation: IConversation =
+      ConversationService.instance.create(persona)
+
+    const prompt = [
+      'Each line should be less than 72 characters.',
+      'Use the following format:',
+      'The changes made in this commit include:',
+      '- Added [file name]',
+      '- Modified [file name] to [brief description of change]',
+      '- Deleted [file name]',
+      'Please summarise the following git diff',
+      diff,
+    ].join('\n')
+
+    const chatCompletion: IChatCompletion = {
+      content: prompt,
+      author: 'vscode-openai-editor',
+      timestamp: new Date().toLocaleString(),
+      mine: false,
+      completionTokens: 0,
+      promptTokens: 0,
+      totalTokens: 0,
+    }
+    conversation.chatMessages.push(chatCompletion)
+    const result = await createChatCompletion(
+      conversation,
+      ResponseFormat.SourceCode
+    )
+    return result?.content ? result?.content : ''
   }
-  return vscodeGit!.exports.getAPI(1)
+  return ''
 }
+
+const getGitDifferences = async (
+  git: GitService
+): Promise<string | undefined> => {
+  const repo = git.getSelectedRepository()
+  let diff = await repo?.diff(true)
+  if (!diff) diff = await repo?.diff(false)
+  return diff
+}
+
+const getDiffs = async (git: GitService): Promise<string[] | undefined> => {
+  const repo = git.getSelectedRepository()
+  let diff = await repo?.diff(true)
+  if (!diff) diff = await repo?.diff(false)
+  return diff?.split('diff --git ')
+}
+
+// function handlerRepositoryDidChange(repositoryInfo: {
+//   numberOfRepositories: number
+//   selectedRepositoryPath: string
+//   availableRepositories: string[]
+// }): void {
+//   showMessageWithTimeout(
+//     `${repositoryInfo.numberOfRepositories} - ${repositoryInfo.selectedRepositoryPath}`
+//   )
+// }

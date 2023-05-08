9241b0922af02fc162dee86c3b5a72c3305b9e98
Andrew Butson <butsona@arbs.io>
2023-05-08 23:19:59 +0200

    The changes made in this commit include: - Deleted openaiEditorCommand.ts - Modified registerOpenaiEditor.ts to use the factory pattern and register commands - Added registerOpenSettings.ts to register a command that opens the settings for vscode-openai.

--- src/contexts/editor/registerOpenaiEditor.ts
+++ src/contexts/editor/registerOpenaiEditor.ts
@@ -1,7 +1,91 @@
-import { ExtensionContext } from 'vscode'
-import { OpenaiEditorCommand } from './openaiEditorCommand'
+import { commands, ExtensionContext, Uri } from 'vscode'
+import { IChatCompletion, IConversation } from '@app/interfaces'
+import { getSystemPersonas } from '@app/models'
 
+import {
+  PromptFactory,
+  BountyPromptFactory,
+  CommentPromptFactory,
+  ExplainPromptFactory,
+  OptimizePromptFactory,
+  PatternPromptFactory,
+  createChatCompletion,
+  ResponseFormat,
+} from '@app/utilities/openai'
+import { compareFileToClipboard, logError } from '@app/utilities/vscode'
+import { VSCODE_OPENAI_PROMPT } from '@app/contexts'
+import { ConversationService } from '@app/services'
+
+// Define a command registry that uses the factory pattern
+class CommandRegistry {
+  private factories: Map<string, PromptFactory>
+
+  constructor() {
+    this.factories = new Map([
+      [
+        VSCODE_OPENAI_PROMPT.PROMPT_COMMENTS_COMMAND_ID,
+        new CommentPromptFactory(),
+      ],
+      [
+        VSCODE_OPENAI_PROMPT.PROMPT_EXPLAIN_COMMAND_ID,
+        new ExplainPromptFactory(),
+      ],
+      [
+        VSCODE_OPENAI_PROMPT.PROMPT_BOUNTY_COMMAND_ID,
+        new BountyPromptFactory(),
+      ],
+      [
+        VSCODE_OPENAI_PROMPT.PROMPT_OPTIMIZE_COMMAND_ID,
+        new OptimizePromptFactory(),
+      ],
+      [
+        VSCODE_OPENAI_PROMPT.PROMPT_PATTERNS_COMMAND_ID,
+        new PatternPromptFactory(),
+      ],
+    ])
+  }
+
+  registerCommands(context: ExtensionContext) {
+    for (const [commandId, factory] of this.factories.entries()) {
+      const commandHandler = async (uri: Uri) => {
+        try {
+          const persona = getSystemPersonas().find(
+            (a) => a.roleName === 'Developer/Programmer'
+          )
+          if (persona) {
+            const conversation: IConversation =
+              ConversationService.instance.create(persona)
+            const prompt = await factory.createPrompt()()
+
+            const chatCompletion: IChatCompletion = {
+              content: prompt,
+              author: 'vscode-openai-editor',
+              timestamp: new Date().toLocaleString(),
+              mine: false,
+              completionTokens: 0,
+              promptTokens: 0,
+              totalTokens: 0,
+            }
+            conversation.chatMessages.push(chatCompletion)
+            const result = await createChatCompletion(
+              conversation,
+              ResponseFormat.SourceCode
+            )
+            compareFileToClipboard(result?.content ? result?.content : '')
+          }
+        } catch (error) {
+          logError(error)
+        }
+      }
+      context.subscriptions.push(
+        commands.registerCommand(commandId, commandHandler)
+      )
+    }
+  }
+}
+
+// Register the commands using the registry
 export function registerOpenaiEditor(context: ExtensionContext) {
-  const registry = new OpenaiEditorCommand()
+  const registry = new CommandRegistry()
   registry.registerCommands(context)
 }

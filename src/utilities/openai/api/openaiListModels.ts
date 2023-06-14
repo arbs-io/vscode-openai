2cb914301cf7823ca922b83049e738b579560164
Andrew Butson <butsona@arbs.io>
2023-06-14 14:10:30 +0200

    The changes made in this commit include: - Added support for all OpenAI GPT models. - Modified the `openaiListModels` function to filter out models that start with "gpt" instead of hardcoding These changes allow the `openaiListModels` function to dynamically filter out all OpenAI GPT models by checking if the model ID starts with "gpt". This ensures that any future GPT models added by OpenAI will be included in the list without requiring manual updates to the code.

--- src/utilities/openai/api/openaiListModels.ts
+++ src/utilities/openai/api/openaiListModels.ts
@@ -14,16 +14,8 @@ export async function openaiListModels(
     const openai = new OpenAIApi(configuration)
     const response = await openai.listModels()
 
-    const chatCompletionModels = [
-      'gpt-4',
-      'gpt-4-0314',
-      'gpt-4-32k',
-      'gpt-4-32k-0314',
-      'gpt-3.5-turbo',
-      'gpt-3.5-turbo-0301',
-    ]
     response.data.data.forEach((model) => {
-      if (chatCompletionModels.includes(model.id)) {
+      if (model.id.startsWith('gpt')) {
         models.push(model.id)
       }
     })

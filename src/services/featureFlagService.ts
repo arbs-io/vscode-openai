cbdb88e016e6e957face19d28728d1cb6514ea5e
Andrew Butson <butsona@arbs.io>
2023-06-27 08:45:35 +0200

    - Modified package.json to add a new "viewsWelcome" section, which provides instructions for using the Query Resources feature with OpenAI service providers. This section is displayed when the embedding feature is not enabled. - Updated the "views" section in package.json to remove the condition that the embedding feature must be enabled for the Query Resources view to be available. - Added a new configuration option "vscode-openai.embeddingModel.show-welcome" in package.json, which controls whether to display a welcome panel for embedding. - Modified src/services/featureFlagService.ts to update the conditions under which the embedding feature flag is set. Now, if either the service provider is 'VSCode-OpenAI' or if an embedding model setup is required, then the flag will be set to false. Otherwise, it will be set to true.

--- src/services/featureFlagService.ts
+++ src/services/featureFlagService.ts
@@ -3,9 +3,12 @@ import { ConfigurationService } from '@app/services'
 import { setFeatureFlag } from '@app/utilities/vscode'
 
 export default function featureFlagService() {
-  if (ConfigurationService.instance.embeddingModel !== 'setup-required') {
-    setFeatureFlag(VSCODE_OPENAI_EMBEDDING.ENABLED_COMMAND_ID, true)
-  } else {
+  if (
+    ConfigurationService.instance.serviceProvider === 'VSCode-OpenAI' ||
+    ConfigurationService.instance.embeddingModel === 'setup-required'
+  ) {
     setFeatureFlag(VSCODE_OPENAI_EMBEDDING.ENABLED_COMMAND_ID, false)
+  } else {
+    setFeatureFlag(VSCODE_OPENAI_EMBEDDING.ENABLED_COMMAND_ID, true)
   }
 }

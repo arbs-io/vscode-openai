f37c1c880034989b63ae980d658201f68433c19f
Andrew Butson <butsona@arbs.io>
2023-07-06 14:07:15 +0200

    - Modified `package.json` to add new commands for "vscode-openai.settings.embedding" in the navigation and settings category. - Modified `src/constants/constants.ts` to add a new constant "SETTINGS_EMBEDDING_COMMAND_ID". - Modified `src/contexts/settings/registerOpenSettings.ts` to register the new command "vscode-openai.settings.embedding" and handle any errors that may occur during its execution.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,6 +7,7 @@ export const VSCODE_OPENAI_EXTENSION = {
   INSTRUMENTATION_KEY: 'e01c0a97-9930-4885-b2e8-772176ced488',
   ENABLED_COMMAND_ID: 'vscode-openai.extension.enabled',
   SETTINGS_PROMPT_EDIT_COMMAND_ID: 'vscode-openai.settings.prompt-editor',
+  SETTINGS_EMBEDDING_COMMAND_ID: 'vscode-openai.settings.embedding',
 }
 
 // VSCODE_OPENAI_REGISTER: Contains the command ID for registering the OpenAI service.
--- src/contexts/settings/registerOpenSettings.ts
+++ src/contexts/settings/registerOpenSettings.ts
@@ -3,10 +3,33 @@ import { VSCODE_OPENAI_EXTENSION } from '@app/constants'
 import { createErrorNotification } from '@app/utilities/node'
 
 export function registerOpenSettings(context: ExtensionContext): void {
+  _registerOpenSettingsEditorPrompt(context)
+  _registerOpenSettingsEmbedding(context)
+}
+
+function _registerOpenSettingsEditorPrompt(context: ExtensionContext): void {
   try {
     context.subscriptions.push(
       commands.registerCommand(
         VSCODE_OPENAI_EXTENSION.SETTINGS_PROMPT_EDIT_COMMAND_ID,
+        async () => {
+          commands.executeCommand(
+            'workbench.action.openSettings',
+            'vscode-openai.prompt-editor'
+          )
+        }
+      )
+    )
+  } catch (error) {
+    createErrorNotification(error)
+  }
+}
+
+function _registerOpenSettingsEmbedding(context: ExtensionContext): void {
+  try {
+    context.subscriptions.push(
+      commands.registerCommand(
+        VSCODE_OPENAI_EXTENSION.SETTINGS_EMBEDDING_COMMAND_ID,
         async () => {
           commands.executeCommand(
             'workbench.action.openSettings',

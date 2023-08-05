328141e7fa62a15e4c231e1b12fa5347031a5e39
Andrew Butson <butsona@arbs.io>
2023-08-05 15:42:09 +0200

    The changes made in this commit include: - Modified package.json to update the version from 1.4.2 to 1.4.3 - Modified src/utilities/embedding/chunk/chunkText.ts to refactor the chunkText function. The function now uses a singleton instance of ChunkBuilder from ChunkBuilderFactory to chunk a string. The previous implementation of the function was removed and replaced with a new implementation that uses the Strategy design pattern. This pattern allows the chunking strategy to be easily switched out for a different one in the future, if needed.

--- src/utilities/embedding/chunk/chunkText.ts
+++ src/utilities/embedding/chunk/chunkText.ts
@@ -1,3 +1,83 @@
+// Define the strategy interface
+// The IChunkingStrategy interface defines a single method, chunk, which takes
+// a string and a maximum character length, and returns an array of strings.
+interface IChunkingStrategy {
+  chunk(content: string, maxCharLength: number): string[]
+}
+
+// SentenceChunkingStrategy is a class that implements the IChunkingStrategy
+// interface. It defines a method, chunk, which splits a string into chunks
+// based on sentence boundaries.
+class SentenceChunkingStrategy implements IChunkingStrategy {
+  chunk(content: string, maxCharLength: number): string[] {
+    const chunks: string[] = []
+    let currentChunk = ''
+    const sentences = content.replace(/\n/g, ' ').split(/([.])/)
+
+    sentences.forEach((sentence) => {
+      const trimmedSentence = sentence.trim()
+      if (!trimmedSentence) return
+
+      const chunkLength = currentChunk.length + trimmedSentence.length + 1
+      const lowerBound = maxCharLength - maxCharLength * 0.5
+      const upperBound = maxCharLength + maxCharLength * 0.5
+
+      if (
+        (chunkLength >= lowerBound &&
+          chunkLength <= upperBound &&
+          currentChunk) ||
+        chunkLength > upperBound
+      ) {
+        currentChunk = currentChunk.replace(/^[. ]+/, '').trim()
+        if (currentChunk) chunks.push(currentChunk)
+        currentChunk = chunkLength > upperBound ? trimmedSentence : ''
+      } else {
+        currentChunk += `${
+          trimmedSentence === '.' ? '' : ' '
+        }${trimmedSentence}`
+      }
+    })
+
+    if (currentChunk) {
+      chunks.push(currentChunk)
+    }
+
+    return chunks
+  }
+}
+
+// Define the builder
+class ChunkBuilder {
+  private strategy: IChunkingStrategy
+
+  constructor(strategy: IChunkingStrategy) {
+    this.strategy = strategy
+  }
+
+  build(content: string, maxCharLength: number): string[] {
+    return this.strategy.chunk(content, maxCharLength)
+  }
+}
+
+// ChunkBuilderFactory is a singleton class that provides a single instance
+// of ChunkBuilder.
+class ChunkBuilderFactory {
+  private static instance: ChunkBuilder
+
+  static getInstance(): ChunkBuilder {
+    if (!ChunkBuilderFactory.instance) {
+      ChunkBuilderFactory.instance = new ChunkBuilder(
+        new SentenceChunkingStrategy()
+      )
+    }
+
+    return ChunkBuilderFactory.instance
+  }
+}
+
+// The chunkText function is a utility function that uses the
+// ChunkBuilderFactory to get an instance of ChunkBuilder, and then uses
+// that instance to chunk a string.
 export function chunkText({
   content,
   maxCharLength,
@@ -5,35 +85,6 @@ export function chunkText({
   content: string
   maxCharLength: number
 }): string[] {
-  const chunks: string[] = []
-  let currentChunk = ''
-  const sentences = content.replace(/\n/g, ' ').split(/([.])/)
-
-  sentences.forEach((sentence) => {
-    const trimmedSentence = sentence.trim()
-    if (!trimmedSentence) return
-
-    const chunkLength = currentChunk.length + trimmedSentence.length + 1
-    const lowerBound = maxCharLength - maxCharLength * 0.5
-    const upperBound = maxCharLength + maxCharLength * 0.5
-
-    if (
-      (chunkLength >= lowerBound &&
-        chunkLength <= upperBound &&
-        currentChunk) ||
-      chunkLength > upperBound
-    ) {
-      currentChunk = currentChunk.replace(/^[. ]+/, '').trim()
-      if (currentChunk) chunks.push(currentChunk)
-      currentChunk = chunkLength > upperBound ? trimmedSentence : ''
-    } else {
-      currentChunk += `${trimmedSentence === '.' ? '' : ' '}${trimmedSentence}`
-    }
-  })
-
-  if (currentChunk) {
-    chunks.push(currentChunk)
-  }
-
-  return chunks
+  const builder = ChunkBuilderFactory.getInstance()
+  return builder.build(content, maxCharLength)
 }

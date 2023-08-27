4597327a6307b1fca666cb24964a6b9d718f0180
Andrew Butson <butsona@arbs.io>
2023-08-27 16:15:02 +0200

    - Modified src/apis/embedding/content/embeddingResource.ts to refactor the way text is extracted from the buffer and how file information is handled. - Deleted src/apis/embedding/content/extractText.ts as its functionality has been moved to embeddingResource.ts. - Modified src/apis/embedding/index.ts to remove the exports for the deleted functions from extractText.ts.

--- src/apis/embedding/index.ts
+++ src/apis/embedding/index.ts
@@ -1,7 +1,3 @@
-export {
-  extractTextFromBuffer,
-  extractTextFromFile,
-} from './content/extractText'
 export { embeddingResource } from './content/embeddingResource'
 export { chunkText } from './chunk/chunkText'
 export { getEmbeddingsForText } from './chunk/getEmbeddingsForText'

40fca5a5a5b8c7d0fc32e364291db8ed284ec55b
Andrew Butson <butsona@arbs.io>
2023-11-26 00:15:34 +0100

    - Added `createErrorNotification` import from `@app/apis/node`. - Modified the `execute` method of the `GenerateCommentsCommand` class to handle the case when `gitService` is not available and display an error notification. - Added an error notification when `gitService` is not available. - Added an error notification when there are no invalid differences in `differences`.

--- src/commands/scm/generateComments.ts
+++ src/commands/scm/generateComments.ts
@@ -1,17 +1,24 @@
 import { Command } from '../commandManager'
 import { GitService, getComments, getGitDifferences } from '@app/apis/git'
+import { createErrorNotification } from '@app/apis/node'
 
 export default class GenerateCommentsCommand implements Command {
   public readonly id = '_vscode-openai.scm.generate.comments'
 
   public async execute() {
     const gitService = new GitService()
-    if (gitService.isAvailable()) {
-      const differences = await getGitDifferences(gitService)
-      if (differences) {
-        const comments = await getComments(differences)
-        gitService.setSCMInputBoxMessage(comments)
-      }
+
+    if (!gitService.isAvailable()) {
+      createErrorNotification(`gitService: service is not available...`)
+      return
+    }
+
+    const differences = await getGitDifferences(gitService)
+    if (differences) {
+      const comments = await getComments(differences)
+      gitService.setSCMInputBoxMessage(comments)
+    } else {
+      createErrorNotification(`gitService: no invalid differences`)
     }
   }
 }

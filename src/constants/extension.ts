f11ef8c6612f9ff9fce033df4669c05dc79b3f6f
Andrew Butson <butsona@arbs.io>
2023-12-18 11:46:19 +0100

    🛠️ src/constants/index.ts -> The constants have been refactored and moved to separate files. The index file now only exports these constants from their respective files.

---
+++ src/constants/extension.ts
@@ -0,0 +1,4 @@
+export const VSCODE_OPENAI_EXTENSION = {
+  INSTRUMENTATION_KEY: 'e01c0a97-9930-4885-b2e8-772176ced488',
+  ENABLED_COMMAND_ID: 'vscode-openai.extension.enabled',
+}
---
+++ src/constants/qpSetup.ts
@@ -0,0 +1,32 @@
+export const VSCODE_OPENAI_QP_SETUP = {
+  PROVIDER_VSCODE: 'vscode-openai',
+  PROVIDER_VSCODE_ICON: 'pulse',
+  PROVIDER_VSCODE_DESC: '(Sponsored) Use vscode-openai service',
+
+  PROVIDER_OPENAI: 'openai.com',
+  PROVIDER_OPENAI_ICON: 'vscode-openai',
+  PROVIDER_OPENAI_DESC:
+    '(BYOK) Use your own OpenAI subscription (api.openai.com)',
+
+  PROVIDER_AZURE: 'openai.azure.com',
+  PROVIDER_AZURE_ICON: 'azure',
+  PROVIDER_AZURE_DESC:
+    '(BYOK) Use your own Azure OpenAI instance (instance.openai.azure.com)',
+
+  PROVIDER_CREDAL: 'credal.ai',
+  PROVIDER_CREDAL_ICON: 'comment',
+  PROVIDER_CREDAL_DESC: '(BYOK) Use your own Credal instance (credal.ai)',
+
+  PROVIDER_CUSTOM: 'custom',
+  PROVIDER_CUSTOM_ICON: 'server-environment',
+  PROVIDER_CUSTOM_DESC:
+    '(BYOI) Use your own instance LLM or SLM (openai-api support required)',
+
+  MODEL_CHANGE: 'change models',
+  MODEL_CHANGE_ICON: 'symbol-function',
+  MODEL_CHANGE_DESC: 'change chat and embedding model',
+
+  CONFIGURATION_RESET: 'reset...',
+  CONFIGURATION_RESET_ICON: 'wrench',
+  CONFIGURATION_RESET_DESC: 'reset vscode-openai configuration',
+}

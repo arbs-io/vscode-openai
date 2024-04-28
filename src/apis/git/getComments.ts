41138838db6d4c1a286d43b54a8b2ba6a363256a
Andrew Butson <butsona@arbs.io>
2024-04-28 20:25:25 +0800

    🛠️ src/apis/git/getComments.ts -> Simplified imports, improved error handling

--- src/apis/git/getComments.ts
+++ src/apis/git/getComments.ts
@@ -1,24 +1,22 @@
-import {
-  IChatCompletion,
-  IChatCompletionConfig,
-  IConversation,
-} from '@app/types'
+import { IChatCompletion, IConversation } from '@app/types'
 import { getSystemPersonas } from '@app/models'
-import {
-  ConfigurationSettingService,
-  ConversationStorageService,
-} from '@app/services'
+import { ConversationStorageService } from '@app/services'
 import { createChatCompletion } from '@app/apis/openai'
 import { VSCODE_OPENAI_QP_PERSONA } from '@app/constants'
 import { workspace } from 'vscode'
+import { ChatCompletionConfigFactory } from '@app/services/configurationServices'
 
 // This function generates comments for the given git differences using OpenAI's chatbot API.
 // It takes a string representing the git differences as input and returns a Promise that resolves to a string.
 export const getComments = async (diff: string): Promise<string> => {
-  const persona = getSystemPersonas().find(
-    (a) => a.roleName === VSCODE_OPENAI_QP_PERSONA.DEVELOPER
-  )
-  if (persona) {
+  try {
+    const persona = getSystemPersonas().find(
+      (a) => a.roleName === VSCODE_OPENAI_QP_PERSONA.DEVELOPER
+    )
+
+    if (!persona) {
+      throw new Error('Developer persona not found.')
+    }
     const conversation: IConversation =
       await ConversationStorageService.instance.create(persona)
 
@@ -26,7 +24,7 @@ export const getComments = async (diff: string): Promise<string> => {
       .getConfiguration('vscode-openai')
       .get(`prompts.persona.scm`) as string
 
-    const prompt = [personaPrompt, diff].join('\n')
+    const prompt = `${personaPrompt}\n${diff}`
 
     const chatCompletion: IChatCompletion = {
       content: prompt,
@@ -39,19 +37,12 @@ export const getComments = async (diff: string): Promise<string> => {
     }
     conversation.chatMessages.push(chatCompletion)
 
-    const chatCompletionConfig: IChatCompletionConfig = {
-      azureApiVersion: ConfigurationSettingService.azureApiVersion,
-      apiKey: ConfigurationSettingService.getApiKey(),
-      baseURL: ConfigurationSettingService.scmUrl,
-      model: ConfigurationSettingService.scmModel,
-      requestConfig: ConfigurationSettingService.getRequestConfig(),
-    }
+    const cfg = ChatCompletionConfigFactory.createConfig('scm_model')
 
-    const result = await createChatCompletion(
-      conversation,
-      chatCompletionConfig
-    )
-    return result?.content ? result?.content : ''
+    const result = await createChatCompletion(conversation, cfg)
+    return result?.content || ''
+  } catch (error) {
+    console.error('Failed to generate comments:', error)
+    return '' // Return empty string in case of error
   }
-  return ''
 }

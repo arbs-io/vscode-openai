b77891eb93715da9133d1ead71709547ce6a4598
Andrew Butson <butsona@arbs.io>
2024-05-01 13:07:07 +0800

    🛠️ CopyToClipboardButton.tsx -> Simplified object props, added comments, enhanced accessibility
    🛠️ OpenSourceFileButton.tsx -> Added types, simplified props, improved documentation

--- webview-ui/messageWebview/src/components/Buttons/OpenSourceFileButton.tsx
+++ webview-ui/messageWebview/src/components/Buttons/OpenSourceFileButton.tsx
@@ -1,23 +1,33 @@
-import { FC } from 'react'
+import { FC, ReactElement } from 'react'
 import { Button, Tooltip } from '@fluentui/react-components'
 import { bundleIcon, Open24Filled, Open24Regular } from '@fluentui/react-icons'
 import { ICodeDocument } from '../../interfaces'
 import { vscode } from '../../utilities/vscode'
 
+// Function to handle the creation of a code document
 const handleCreateCodeDocument = (language: string, content: string) => {
   const codeDocument: ICodeDocument = {
-    language: language,
-    content: content,
+    language,
+    content,
   }
+  // Sending a message to VSCode or similar environment to handle document creation
   vscode.postMessage({
     command: 'onDidCreateDocument',
     text: JSON.stringify(codeDocument),
   })
 }
 
+// Bundling icons for different states
 const OpenIcon = bundleIcon(Open24Filled, Open24Regular)
 
-const OpenSourceFileButton: FC<ICodeDocument> = ({ language, content }) => {
+// Defining props specifically for this component for clarity and future-proofing
+interface OpenSourceFileButtonProps extends ICodeDocument {}
+
+// Functional component for opening a source file
+const OpenSourceFileButton: FC<OpenSourceFileButtonProps> = ({
+  language,
+  content,
+}): ReactElement => {
   return (
     <Tooltip content="Open code in new source code file" relationship="label">
       <Button

ade7f2ecf5b2346934870ae0e1f9ac097eb71155
Andrew Butson <butsona@arbs.io>
2025-03-01 18:47:06 +0800

    🛠️ src/commands/embeddings/index.ts -> Added new export 🛠️ src/commands/index.ts -> Registered new command

---
+++ src/commands/embeddings/deleteAll.ts
@@ -0,0 +1,22 @@
+import { window } from 'vscode'
+import { EmbeddingTreeDataProvider, EmbeddingTreeItem } from '@app/providers'
+import { ICommand } from '@app/commands'
+import { EmbeddingStorageService } from '@app/services'
+
+export default class RefreshCommand implements ICommand {
+  public readonly id = '_vscode-openai.embeddings.delete-all'
+
+  public async execute() {
+    window
+      .showInformationMessage(
+        'Are you sure you want to delete ALL embeddings?',
+        'Yes',
+        'No'
+      )
+      .then((answer) => {
+        if (answer === 'Yes') {
+          EmbeddingStorageService.instance.deleteAll()
+        }
+      })
+  }
+}

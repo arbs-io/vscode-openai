88b7f3f71646db2d76d6aeac8713aa422e12f7fc
Chiu, Alex (US) - CHQ <alex.chiu@L3Harris.com>
2025-03-04 17:05:59 -0800

    Enable Azure authentication against US Sovereign Cloud
    - Add extension option to point Azure to US Gov cloud endpoint
    - Still defaults to commercial cloud

--- src/apis/vscode/authentication/getAzureOpenAIAccessToken.ts
+++ src/apis/vscode/authentication/getAzureOpenAIAccessToken.ts
@@ -1,9 +1,12 @@
 import { authentication } from 'vscode'
+import {
+  SettingConfig as settingCfg,
+} from '@app/services'
 
 export async function getAzureOpenAIAccessToken(): Promise<string> {
   const msSession = await authentication.getSession(
-    'microsoft',
-    ['https://cognitiveservices.azure.com/.default', 'offline_access'],
+    settingCfg.azureEnvironment,
+    [settingCfg.azureEndpoint, 'offline_access'],
     { createIfNone: true }
   )
   return msSession.accessToken

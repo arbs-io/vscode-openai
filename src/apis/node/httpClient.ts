ad0fe69c9d9b678162da48b168a24e56de6b396c
Andrew Butson <butsona@arbs.io>
2025-03-08 07:09:19 +0000

    🛠️ src/apis/node/httpClient.ts -> Import https as module 🛠️ src/apis/node/httpClient.ts -> Update comments for clarity 🛠️ src/apis/node/httpClient.ts -> Fix TypeScript typo

--- src/apis/node/httpClient.ts
+++ src/apis/node/httpClient.ts
@@ -1,14 +1,15 @@
 import { SettingConfig as settingCfg } from '@app/services';
 import { ClientRequest } from 'http';
-import http = require('https');
+import * as https from 'https';
 import { Uri } from 'vscode';
 
 /**
- * https request class in typesript to show how to manage errors and events
+ * https request class in TypeScript to show how to manage errors and events
  * to help prevent ECONNRESET errors
  */
 export class HttpRequest {
-  private _requestOptions: http.RequestOptions = {};
+  private _requestOptions: https.RequestOptions = {};
+
   constructor(method: string, apiKey: string, baseUrl: string) {
     const headers = settingCfg.apiHeaders;
     const uri = Uri.parse(baseUrl);
@@ -26,10 +27,11 @@ export class HttpRequest {
       },
     };
   }
+
   public async send(data?: any): Promise<any> {
     let result = '';
     const promise = new Promise((resolve, reject) => {
-      const req: ClientRequest = http.request(this._requestOptions, (res) => {
+      const req: ClientRequest = https.request(this._requestOptions, (res) => {
         res.on('data', (chunk) => {
           result += chunk;
         });
@@ -41,7 +43,7 @@ export class HttpRequest {
         res.on('end', () => {
           try {
             let body = result;
-            //there are empty responses
+            // there are empty responses
 
             if (res.statusCode === 200) {
               body = JSON.parse(result);
@@ -55,28 +57,28 @@ export class HttpRequest {
       });
 
       /***
-       * handles the errors on the request
+       * Handles the errors on the request
        */
       req.on('error', (_err) => {
         reject(new Error('errors on the request'));
       });
 
       /***
-       * handles the timeout error
+       * Handles the timeout error
        */
       req.on('timeout', (_err: any) => {
         req.abort();
       });
 
       /***
-       * unhandle errors on the request
+       * Handles uncaught exceptions on the request
        */
       req.on('uncaughtException', () => {
         req.abort();
       });
 
       /**
-       * adds the payload/body
+       * Adds the payload/body
        */
       if (data) {
         const body = JSON.stringify(data);
@@ -84,7 +86,7 @@ export class HttpRequest {
       }
 
       /**
-       * end the request to prevent ECONNRESETand socket hung errors
+       * Ends the request to prevent ECONNRESET and socket hung errors
        */
       req.end(() => {
         // createDebugNotification(`HttpRequest: ${req.method}::${req.host}`)
